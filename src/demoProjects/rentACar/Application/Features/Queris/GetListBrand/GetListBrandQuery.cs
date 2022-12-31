bf583d127a02f08165408d33f84395bd59d1ce14
Mikayil Aktemur <mikayil34@gmail.com>
2022-12-31 11:02:56 +0300

    add

---
+++ src/demoProjects/rentACar/Application/Features/Queris/GetListBrand/GetListBrandQuery.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Queris.GetListBrand
+{
+    public class GetListBrandQuery: IRequest<BrandListModel>
+    {
+
+
+    }
+}
